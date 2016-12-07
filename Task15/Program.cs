2c6e6e564d873ed36db6f1b7e9ea965a6ea612b6
Mladen Mijailovic <mladenm17@gmail.com>
2016-12-07 19:31:35 +0100

    First commit

---
+++ Task15/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task15
+{
+    class Program
+    {
+        // I used ASCII Alphabet Characters for this one
+        //they are [A - Z] in decimal [65 - 90]
+
+        private static char Ceasar(char chr)
+        {
+            if (chr == ' ') 
+                return ' ';
+            if (chr == ' ')
+                return ' ';
+            
+            int shift = chr;
+            shift += 3;
+            if (shift > 90) shift -= 26;
+            return (char)shift;
+
+        }
+
+        public static string Ceasar(string str)
+        {
+            string RetStr = "";
+            foreach (char c in str.ToCharArray())
+                RetStr += Ceasar(c).ToString();
+            return RetStr;
+        }
+
+        static void Main(string[] args)
+        {
+            string g = "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD";
+            Console.WriteLine("Ceasar cipher coded: " + g);
+            Console.WriteLine();
+            string ceaserDeshifted = Ceasar(g);
+            Console.WriteLine("Decoded: " + ceaserDeshifted);
+
+            Console.ReadKey();
+        }
+    }
+}
